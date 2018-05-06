08b1f688b45171b6f0ccd91d183115a7e473252a
Jacob Reed <jacob.russell.reed@gmail.com>
2018-05-06 16:51:22 -0700

    Adding connections

---
+++ routes/friends.js
@@ -0,0 +1,54 @@
+//express is the framework we're going to use to handle requests
+const express = require('express');
+//Create a new instance of express
+const app = express();
+
+const FormData = require("form-data");
+
+const bodyParser = require("body-parser");
+//This allows parsing of the body of POST requests, that are encoded in JSON
+app.use(bodyParser.json());
+
+//Create connection to Heroku Database
+let db = require('../utilities/utils').db;
+
+let getHash = require('../utilities/utils').getHash;
+
+var router = express.Router();
+
+//app.get('/users') means accept http 'GET' requests at path '/users'
+router.post('/', (req, res) => {
+    let user = req.body['username'];
+    let wasSuccessful = false;
+    if (user) {
+        //Using the 'one' method means that only one row should be returned
+        db.one('SELECT memberid FROM Members WHERE username=$1', [user])
+            //If successful, run function passed into .then()
+            .then(row => {
+                let memberID = row['memberid'];
+                db.manyOrNone('SELECT memberid_b FROM Contacts WHERE memberid_a=$1 UNION SELECT memberid_a FROM Contacts WHERE memberid_b=$1', [memberID]);
+                .then(row => {
+                    let friends = row['memberid'];
+                    res.send({
+                        success: true,
+                        friend: freinds
+                    });
+                });
+            })
+            //More than one row shouldn't be found, since table has constraint on it
+            .catch((err) => {
+                //If anything happened, it wasn't successful
+                res.send({
+                    success: false,
+                    message: err
+                });
+            });
+    } else {
+        res.send({
+            success: false,
+            message: 'missing credentials'
+        });
+    }
+});
+
+module.exports = router;
