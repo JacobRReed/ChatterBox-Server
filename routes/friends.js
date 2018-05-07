cf7359e258ac1f8de69a3141c70d6bb78501e113
Jacob Reed <jacob.russell.reed@gmail.com>
2018-05-06 17:17:11 -0700

    a

--- routes/friends.js
+++ routes/friends.js
@@ -18,41 +18,41 @@ var router = express.Router();
 
 //app.get('/users') means accept http 'GET' requests at path '/users'
 router.post('/', (req, res) => {
-            let user = req.body['username'];
+    let user = req.body['username'];
 
-            console.log(user);
+    console.log(user);
 
-            if (user) {
-                //Using the 'one' method means that only one row should be returned
-                db.one('SELECT memberid FROM Members WHERE username LIKE $1', [user])
-                    //If successful, run function passed into .then()
+    if (user) {
+        //Using the 'one' method means that only one row should be returned
+        db.one('SELECT memberid FROM Members WHERE username LIKE $1', [user])
+            //If successful, run function passed into .then()
+            .then(row => {
+                let memberID = row['memberid'];
+                db.manyOrNone('SELECT memberid_b FROM Contacts WHERE memberid_a=$1 UNION SELECT memberid_a FROM Contacts WHERE memberid_b=$1', [memberID])
                     .then(row => {
-                            let memberID = row['memberid'];
-                            db.manyOrNone('SELECT memberid_b FROM Contacts WHERE memberid_a=$1 UNION SELECT memberid_a FROM Contacts WHERE memberid_b=$1', [memberID])
-                                .then(row => {
-                                        console.log("Friends: " + row);
-                                        let usernamesOfFriends = [];
-                                        for (i = 0; i < row.length; i++) {
-                                            db.manyOrNone('SELECT username FROM Members WHERE memberid=$1', [row[i]])
-                                                .then(name => {
-                                                        usernamesOfFriends.push(name);
-                                                    }
-                                                }
-                                            console.log(usernamesOfFriends);
-                                            res.send({
-                                                friends: usernamesOfFriends
-                                            });
-                                        });
-                                })
-                        //More than one row shouldn't be found, since table has constraint on it
-                        .catch((err) => {
-                            //If anything happened, it wasn't successful
-                            res.send({
-                                success: false,
-                                message: err
-                            });
+                        console.log("Friends: " + row);
+                        let usernamesOfFriends = [];
+                        for (i = 0; i < row.length; i++) {
+                            db.manyOrNone('SELECT username FROM Members WHERE memberid=$1', [row[i]])
+                                .then(name => {
+                                    usernamesOfFriends.push(name);
+                                });
+                        }
+                        console.log(usernamesOfFriends);
+                        res.send({
+                            friends: usernamesOfFriends
                         });
-                    }
+                    });
+            })
+            //More than one row shouldn't be found, since table has constraint on it
+            .catch((err) => {
+                //If anything happened, it wasn't successful
+                res.send({
+                    success: false,
+                    message: err
+                });
             });
+    }
+});
 
-        module.exports = router;
+module.exports = router;
