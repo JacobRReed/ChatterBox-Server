65b6978db773db3a12a422971ea15f4d75f61588
Keldon Fischer <keldonf@Keldons-MacBook-Pro.local>
2018-05-13 23:59:36 -0700

    Add some new chat methods
    ...

--- routes/chats.js
+++ routes/chats.js
@@ -32,14 +32,14 @@ router.post("/makeChat", (req, res) => {
 });
 
 router.get("/getChat", (req, res) => {
-  let chatId = req.query['chatId'];
-  let after = req.query['after'];
-  let query = `SELECT Distinct(Chats.Name)
-    FROM Chats, Messages
-    INNER JOIN Members ON Messages.MemberId=Members.MemberId
-    WHERE Chats.ChatId=Messages.ChatId
-    ORDER BY Chats.Name ASC`
-  db.manyOrNone(query, [after, chatId])
+  let name = req.query['name']
+  let query = `SELECT chatID FROM CHATS WHERE NAME = $1`
+  // let query = `SELECT Distinct(Chats.Name)
+  //   FROM Chats, Messages
+  //   INNER JOIN Members ON Messages.MemberId=Members.MemberId
+  //   WHERE Chats.ChatId=Messages.ChatId
+  //   ORDER BY Chats.Name ASC`
+  db.manyOrNone(query, [name])
   .then((rows) => {
     res.send({
       messages: rows
@@ -51,5 +51,30 @@ router.get("/getChat", (req, res) => {
     })
   });
 });
+
+
+router.get("/getChat", (req, res) => {
+  let name = req.query['name']
+  let query = `SELECT chatID FROM CHATS WHERE NAME = $1`
+  // let query = `SELECT Distinct(Chats.Name)
+  //   FROM Chats, Messages
+  //   INNER JOIN Members ON Messages.MemberId=Members.MemberId
+  //   WHERE Chats.ChatId=Messages.ChatId
+  //   ORDER BY Chats.Name ASC`
+  db.one(query, [name])
+  .then((row) => {
+    let chatId = row['chatID']
+    res.send({
+      name: chatId
+    })
+  }).catch((err) => {
+    res.send({
+      success: false,
+      error: err
+    })
+  });
+});
+
+
 
 module.exports = router;
