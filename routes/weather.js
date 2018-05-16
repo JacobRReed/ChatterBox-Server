56c1e4bf42a80d6a2ce3c17f10a8bbb89c3de1a1
Jacob Reed <jacob.russell.reed@gmail.com>
2018-05-15 21:48:28 -0700

    a

--- routes/weather.js
+++ routes/weather.js
@@ -10,13 +10,20 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 var router = express.Router();
 
+const weatherKey = process.env.WEATHER_KEY;
 const AccuWeather = require('accuweather');
-const weather = new AccuWeather(process.env.WEATHER_KEY);
+const forecast = new AccuWeather(weatherKey); //Unique client code used for identification and authorization purposes. Contact AccuWeather to receive an API key.
 
-router.post('/', (req, res) => {
-    weather.localkey(28580).details(true).get().then(res => {
-        console.log(res);
-    }).catch(err => {
-        console.log(err);
-    });
-});
+forecast
+    .localkey(28580) // http://apidev.accuweather.com/developers/locationsAPIguide
+    .time('hourly/1hour') // http://apidev.accuweather.com/developers/forecastsAPIguide
+    .language("ru") // http://apidev.accuweather.com/developers/languages
+    .metric(true) // Boolean value (true or false) that specifies to return the data in either metric (=true) or imperial units 
+    .details(true) // Boolean value (true or false) that specifies whether or not to include a truncated version of the forecasts object or the full object (details = true)
+    .get()
+    .then(res => {
+        console.log(res)
+    })
+    .catch(err => {
+        console.log(err)
+    })
