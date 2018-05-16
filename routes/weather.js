b3b41ca6cc54f3272a7920da22809461b404f9aa
Jacob Reed <jacob.russell.reed@gmail.com>
2018-05-15 22:02:29 -0700

    a

--- routes/weather.js
+++ routes/weather.js
@@ -2,28 +2,43 @@
 const express = require('express');
 //Create a new instance of express
 const app = express();
-
+const https = require('https');
 const FormData = require("form-data");
-
 const bodyParser = require("body-parser");
+
 //This allows parsing of the body of POST requests, that are encoded in JSON
 app.use(bodyParser.json());
 var router = express.Router();
 
+//AccuWeather API key
 const weatherKey = process.env.WEATHER_KEY;
-const AccuWeather = require('accuweather');
-const forecast = new AccuWeather(weatherKey); //Unique client code used for identification and authorization purposes. Contact AccuWeather to receive an API key.
 
-forecast
-    .localkey(28580) // http://apidev.accuweather.com/developers/locationsAPIguide
-    .time('hourly/1hour') // http://apidev.accuweather.com/developers/forecastsAPIguide
-    .language("ru") // http://apidev.accuweather.com/developers/languages
-    .metric(true) // Boolean value (true or false) that specifies to return the data in either metric (=true) or imperial units 
-    .details(true) // Boolean value (true or false) that specifies whether or not to include a truncated version of the forecasts object or the full object (details = true)
-    .get()
-    .then(res => {
-        console.log(res)
-    })
-    .catch(err => {
-        console.log(err)
-    })
+//Temp for now
+var latLon = "40.8,-77.8";
+
+//Url to get city code based on lat and lon
+var latLongCityCodeURL = ("https://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=" + weatherKey + "&q=" + latLon);
+https.get(latLongCityCodeURL, (resp) => {
+    let data = '';
+    resp.on('data', (chunk) => {
+        data += chunk;
+    });
+    resp.on('end', () => {
+        console.log(JSON.parse(data).explanation);
+    });
+}).on("error", (err) => {
+    console.log("Error: " + err.message);
+});
+
+var cityCode = 0;
+var currentConditionsURL = ("https://dataservice.accuweather.com/currentconditions/v1/" + cityCode + "?apikey=" + weatherKey);
+
+
+router.post('/', (req, res) => {
+    res.send({
+        success: true
+    });
+});
+
+
+module.exports = router;
