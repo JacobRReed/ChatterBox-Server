e06ef50704a5a03694edf8c1d667c3b0a62f601d
Jacob Reed <jacob.russell.reed@gmail.com>
2018-05-15 21:43:15 -0700

    Weather implementation

---
+++ routes/weather.js
@@ -0,0 +1,20 @@
+//express is the framework we're going to use to handle requests
+const express = require('express');
+//Create a new instance of express
+const app = express();
+
+const FormData = require("form-data");
+
+const bodyParser = require("body-parser");
+//This allows parsing of the body of POST requests, that are encoded in JSON
+app.use(bodyParser.json());
+var router = express.Router();
+
+const AccuWeather = require('accuweather');
+const weather = new AccuWeather(process.env.WEATHER_KEY);
+
+weather.localkey(28580).details(true).get().then(res => {
+    console.log(res);
+}).catch(err => {
+    console.log(err);
+});
