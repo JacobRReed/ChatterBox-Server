2a0a76ffeefc80dc8e04bf87cef76ac39b875a21
Keldon Fischer <keldonf@D-10-16-5-28.dhcp4.washington.edu>
2018-05-24 10:56:16 -0700

    ....

--- routes/messages.js
+++ routes/messages.js
@@ -54,6 +54,29 @@ router.get("/getMessages", (req, res) => {
   });
 });
 
+router.get("/getMessages3", (req, res) => {
+  let chatId = req.query['chatId'];
+  let after = req.query['after'];
+  let query = `SELECT Members.Username, Messages.Message, to_char(Messages.Timestamp AT TIME ZONE 'PDT', 'YYYY-MM-DD
+HH24:MI:SS.US' )
+AS Timestamp
+FROM Messages
+INNER JOIN Members ON Messages.MemberId=Members.MemberId WHERE ChatId=$2 AND
+Timestamp AT TIME ZONE 'PDT' > $1
+ORDER BY Timestamp ASC`
+  db.manyOrNone(query, [after, chatId])
+    .then((rows) => {
+      res.send({
+        messages: rows
+      })
+    }).catch((err) => {
+      res.send({
+        success: false,
+            error: err
+        })
+    });
+});
+
 
 router.get("/getMessages2", (req, res) => {
   let chatId = req.query['chatId'];
