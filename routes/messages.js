9c3b221569edcdc4494c289b2655765497288bbc
Jacob Reed <jacob.russell.reed@gmail.com>
2018-05-29 11:41:30 -0700

    Fixed messages endpoint for get messages

--- routes/messages.js
+++ routes/messages.js
@@ -31,78 +31,9 @@ router.post("/sendMessages", (req, res) => {
   });
 });
 
-router.get("/getMessages4", (req, res) => {
-  let chatId = req.query['chatId'];
-  let query = 'SELECT MESSAGE, TIMESTAMP, MEMBERID FROM MESSAGES WHERE CHATID = $1 ORDER BY TIMESTAMP ASC'
-  db.manyOrNone(query, [chatId])
-  .then((rows) => {
-    res.send({
-      messages: rows
-    })
-  }).catch((err) => {
-    res.send({
-      success: false,
-      error: err
-    })
-  });
-});
-
 router.get("/getMessages", (req, res) => {
   let chatId = req.query['chatId'];
-  let after = req.query['after'];
-  let query = `SELECT Members.Username, Messages.Message, to_char(Messages.Timestamp AT TIME ZONE 'PDT', 'YYYY-MM-DD
-    HH24:MI:SS.US' )
-    AS Timestamp
-    FROM Messages
-    INNER JOIN Members ON Messages.MemberId=Members.MemberId WHERE ChatId=$2
-    Timestamp AT TIME ZONE 'PDT' > $1
-    ORDER BY Timestamp ASC`
-  db.manyOrNone(query, [after, chatId])
-  .then((rows) => {
-    res.send({
-      messages: rows
-    })
-  }).catch((err) => {
-    res.send({
-      success: false,
-      error: err
-    })
-  });
-});
-
-router.get("/getMessages3", (req, res) => {
-  let chatId = req.query['chatId'];
-  let after = req.query['after'];
-  let query = `SELECT Members.Username, Messages.Message, to_char(Messages.Timestamp AT TIME ZONE 'PDT', 'YYYY-MM-DD
-HH24:MI:SS.US' )
-AS Timestamp
-FROM Messages
-INNER JOIN Members ON Messages.MemberId=Members.MemberId WHERE ChatId=$2 AND
-Timestamp AT TIME ZONE 'PDT' > $1
-ORDER BY Timestamp ASC`
-  db.manyOrNone(query, [after, chatId])
-    .then((rows) => {
-      res.send({
-        messages: rows
-
-      })
-    }).catch((err) => {
-      res.send({
-        success: false,
-            error: err
-        })
-    });
-});
-
-
-router.get("/getMessages2", (req, res) => {
-  let chatId = req.query['chatId'];
-  let query = `SELECT Members.Username, Messages.Message, to_char(Messages.Timestamp AT TIME ZONE 'PDT', 'YYYY-MM-DD
-    HH24:MI:SS.US' )
-    AS Timestamp
-    FROM Messages
-    INNER JOIN Members ON Messages.MemberId=Members.MemberId WHERE ChatId=$1
-    ORDER BY Timestamp ASC`
+  let query = 'SELECT MESSAGE, TIMESTAMP, MEMBERID FROM MESSAGES WHERE CHATID = $1 ORDER BY TIMESTAMP ASC'
   db.manyOrNone(query, [chatId])
   .then((rows) => {
     res.send({
